89cd87ee6d1e6d78812ab60dc1e3520ffd55e5d0
shikhardatadays <shikhar@datadays.in>
2023-12-17 19:17:19 +0530

    change in url

--- frontend/src/utils/apiURL.jsx
+++ frontend/src/utils/apiURL.jsx
@@ -1,12 +1,11 @@
 const ENV = import.meta.env.VITE_ENV;
-
-let URL = "http://localhost:5174";
+let URL ;
 
 if (ENV === "LOCAL") {
   URL = "http://localhost:5174";
   console.log("USING ENV: LOCAL");
 } else if (ENV === "DEV") {
-  URL = "";
+  URL = import.meta.env.VITE_URL;
   console.log("USING ENV: DEV");
 } else {
   console.log(
